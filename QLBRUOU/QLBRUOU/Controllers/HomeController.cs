be37bd6f89f57494b7082c9186b880f2b7c28153
baotubi <45580906+baotubi@users.noreply.github.com>
2020-06-01 20:25:12 +0700

    cap nhat home controller & timkiem controller

--- QLBRUOU/QLBRUOU/Controllers/HomeController.cs
+++ QLBRUOU/QLBRUOU/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using QLBRUOU.Models;
 
 namespace QLBRUOU.Controllers
 {
@@ -11,7 +10,6 @@ namespace QLBRUOU.Controllers
     {
         public ActionResult Index()
         {
-           
             return View();
         }
 
